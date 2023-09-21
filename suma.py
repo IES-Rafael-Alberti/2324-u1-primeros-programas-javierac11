d9235b41396ab5eef41d28c43f69f03f8e29f64f
javierac11 <javieracedocastrillon@gmail.com>
2023-09-21 10:21:26 +0200

    Suma

---
+++ suma.py
@@ -0,0 +1,12 @@
+def suma(numero1: int, numero2: int):
+        suma = numero1+numero2 
+        print("Resultado :", suma)
+
+
+x = int(input())
+y = int(input())
+
+x = int(x)
+y = int(y)
+
+suma(x, y)
